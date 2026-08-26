6ff7f9f76818dd3c1e62ccf6238bf7cdc501131f
Issa-barry <issabarry67@gmail.com>
2026-08-26 12:18:25 +0200

    carte1

---
+++ pages/produits.vue
@@ -0,0 +1,14 @@
+<script setup>
+definePageMeta({
+  layout: "landing",
+});
+</script>
+
+<template>
+  <LandingContainer>
+    <LandingSectionhead>
+      <template #title>Nos produits</template>
+      <template #desc>Cette page est en cours de préparation, revenez bientôt.</template>
+    </LandingSectionhead>
+  </LandingContainer>
+</template>
